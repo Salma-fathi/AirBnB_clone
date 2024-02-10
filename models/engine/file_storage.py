d731a45c5524e29f6f1c177461cde4b5af142f98
Salma-fathi <67196308+Salma-fathi@users.noreply.github.com>
2024-02-11 00:17:17 +0300

    go to test

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,73 +3,76 @@
 import json
 import datetime
 
-class FileStorage :
-        """Represent an abstracted storage engine.
+class FileStorage:
+    """Represent an abstracted storage engine.
 
-Attributes:
+    Attributes:
         __file_path (str): The name of the file to save objects to.
         __objects (dict): A dictionary of instantiated objects.
-"""
-def all (self):
+    """
+
+    def all(self):
         """Returns __objects dictionary."""
         return FileStorage.__objects__
 
-def new(self, obj):
+    def new(self, obj):
         """Sets new obj in __objects dictionary."""
         key = "{}.{}".format(type(obj).__name__, obj.id)
         FileStorage.__objects[key] = obj
         return obj
-def save(self):
+
+    def save(self):
         "serializes __objects to the JSON file "
         with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
-                d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
-                json.dump(d, f)
+            d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
+            json.dump(d, f)
 
-def reload(self):
+    def reload(self):
         """Deserialize the JSON file __file_path to __objects, if it exists."""
         try:
-                with open(FileStorage.__file_path) as f:
-                        objdict = json.load(f)
-                for o in objdict.values():
-                        cls_name = o["__class__"]
-                        del o["__class__"]
-                        self.new(eval(cls_name)(**o))
+            with open(FileStorage.__file_path) as f:
+                objdict = json.load(f)
+            for o in objdict.values():
+                cls_name = o["__class__"]
+                del o["__class__"]
+                self.new(eval(cls_name)(**o))
         except FileNotFoundError:
-                return
-def attributes(self):
+            return
+
+    def attributes(self):
         """Returns the valid attributes and their types for classname."""
         attributes = {
-                "BaseModel":
-                        {"id": str,
-                        "created_at": datetime.datetime,
-                        "updated_at": datetime.datetime},
-                "User":
-                        {"email": str,
-                        "password": str,
-                    "first_name": str,
-                    "last_name": str},
+            "BaseModel":
+                {"id": str,
+                 "created_at": datetime.datetime,
+                 "updated_at": datetime.datetime},
+            "User":
+                {"email": str,
+                 "password": str,
+                 "first_name": str,
+                 "last_name": str},
             "State":
-                        {"name": str},
+                {"name": str},
             "City":
-                        {"state_id": str,
-                    "name": str},
+                {"state_id": str,
+                 "name": str},
             "Amenity":
-                        {"name": str},
+                {"name": str},
             "Place":
-                        {"city_id": str,
-                    "user_id": str,
-                    "name": str,
-                    "description": str,
-                    "number_rooms": int,
-                    "number_bathrooms": int,
-                    "max_guest": int,
-                    "price_by_night": int,
-                    "latitude": float,
-                    "longitude": float,
-                    "amenity_ids": list},
-                "Review":
+                {"city_id": str,
+                 "user_id": str,
+                 "name": str,
+                 "description": str,
+                 "number_rooms": int,
+                 "number_bathrooms": int,
+                 "max_guest": int,
+                 "price_by_night": int,
+                 "latitude": float,
+                 "longitude": float,
+                 "amenity_ids": list},
+            "Review":
                 {"place_id": str,
-                        "user_id": str,
-                        "text": str}
+                 "user_id": str,
+                 "text": str}
         }
         return attributes
