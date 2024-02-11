d41bc81137f3ed60405d3eb6d17ca8f2f59a6597
Salma-fathi <67196308+Salma-fathi@users.noreply.github.com>
2024-02-11 17:21:00 +0300

    dele

--- models/base_model.py
+++ models/base_model.py
@@ -32,7 +32,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)  # as instructed also in task 5
+            models.storage.new(self)
 
     def __str__(self):
         """Returns a readable string representation
