3104c516418c8ba2ae7a3dccd66848545b4a708f
Salma-fathi <67196308+Salma-fathi@users.noreply.github.com>
2024-02-11 04:01:43 +0300

    amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,52 +1,82 @@
-#!/usr/bin/python3
-"""Unittest Amenity Class"""
+#!/usr/bin/env python3
+"""Unittest module for the Amenity Class."""
 
 import unittest
-from datetime import datetime
-import time
+import os
 from models.amenity import Amenity
+from models.base_model import BaseModel
+import uuid
+import datetime
+import time
 import re
 import json
 from models.engine.file_storage import FileStorage
-import os
 from models import storage
-from models.base_model import BaseModel
-
 
 class TestAmenity(unittest.TestCase):
+    """Amenity model class test case"""
 
-    """Test Cases for the Amenity class."""
+    @classmethod
+    def setUpClass(cls):
+        """Setup the unittest"""
+        cls.amenity = Amenity()
+        cls.amenity.name = "Wifi"
 
-    def setUp(self):
-        """Sets up test methods."""
-        pass
+    @classmethod
+    def tearDownClass(cls):
+        """Clean up the dirt"""
+        del cls.amenity
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
 
-    def tearDown(self):
-        """Tears down test methods."""
-        self.resetStorage()
-        pass
+    def test_is_subclass(self):
+        self.assertTrue(issubclass(self.amenity.__class__, BaseModel))
 
-    def resetStorage(self):
-        """Resets FileStorage data."""
-        FileStorage._FileStorage__objects = {}
-        if os.path.isfile(FileStorage._FileStorage__file_path):
-            os.remove(FileStorage._FileStorage__file_path)
+    def checking_for_doc(self):
+        self.assertIsNotNone(Amenity.__doc__)
 
-    def testtinstantiation(self):
-        """Tests instantiation of Amenity class."""
+    # def test_has_attributes(self):
+    #    self.assertTrue('id' in self.amenity.__dict__)
+    #    self.assertTrue('created_at' in self.amenity.__dict__)
+    #    self.assertTrue('updated_at' in self.amenity.__dict__)
+    #    self.assertTrue('name' in self.amenity.__dict__)
+    # OR
+    a = Amenity()
+    def test_has_attributes(self):
+        """verify if attributes exist"""
+        self.assertTrue(hasattr(self.a, 'name'))
+        self.assertTrue(hasattr(self.a, 'id'))
+        self.assertTrue(hasattr(self.a, 'created_at'))
+        self.assertTrue(hasattr(self.a, 'updated_at'))
 
-        b = Amenity()
-        self.assertEqual(str(type(b)), "<class 'models.amenity.Amenity'>")
-        self.assertIsInstance(b, Amenity)
-        self.assertTrue(issubclass(type(b), BaseModel))
+    def test_attributes_are_string(self):
+        self.assertIs(type(self.amenity.name), str)
 
-    def testtattributes(self):
-        """Tests the attributes of Amenity class."""
-        attributes = storage.attributes()["Amenity"]
-        o = Amenity()
-        for k, v in attributes.items():
-            self.assertTrue(hasattr(o, k))
-            self.assertEqual(type(getattr(o, k, None)), v)
+    def test_class_exists(self):
+        """tests if class exists"""
+        res = "<class 'models.amenity.Amenity'>"
+        self.assertEqual(str(type(self.a)), res)
+
+    def test_user_inheritance(self):
+        """test if Amenity is a subclass of BaseModel"""
+        self.assertIsInstance(self.a, Amenity)
+
+    def test_types(self):
+        """tests if the type of the attribute is the correct one"""
+        self.assertIsInstance(self.a.name, str)
+        self.assertIsInstance(self.a.id, str)
+        self.assertIsInstance(self.a.created_at, datetime.datetime)
+        self.assertIsInstance(self.a.updated_at, datetime.datetime)
+
+    def test_save(self):
+        self.amenity.save()
+        self.assertNotEqual(self.amenity.created_at, self.amenity.updated_at)
+
+    def test_to_dict(self):
+        self.assertTrue('to_dict' in dir(self.amenity))
+
 
 if __name__ == "__main__":
     unittest.main()
